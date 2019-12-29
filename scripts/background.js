24fc6d7c4d08410bd2cc43e79d7dacd0e408dc41
Serafin Wesnidge <leaf466@gmail.com>
2019-12-28 16:32:50 -0800

    fix: Enables extension use on any domain

--- scripts/background.js
+++ scripts/background.js
@@ -1,19 +1,3 @@
-chrome.runtime.onInstalled.addListener(function() {
-  chrome.storage.sync.get(['custom_scripts'], function(result) {
-    console.log('Value currently is ' + result.key)
-  })
-  chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
-    chrome.declarativeContent.onPageChanged.addRules([{
-      conditions: [
-        new chrome.declarativeContent.PageStateMatcher({
-          pageUrl: {hostEquals: 'developer.chrome.com'},
-        })
-      ],
-      actions: [new chrome.declarativeContent.ShowPageAction()]
-    }])
-  })
-})
-
 chrome.runtime.onMessage.addListener(function(message, callback) {
   const messages = {
     hello: () => {
